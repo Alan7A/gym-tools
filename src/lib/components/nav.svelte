3f372110f35986c0458b9d4edabdb45a6cf1169b
Alan Delgado <7a.alan@gmail.com>
2025-06-01 00:36:39 -0600

    Enhance navigation and add input component
    - Updated the `Nav` component to dynamically set the active tab based on the current page URL.
    - Improved tab trigger styling for better user experience and consistency.
    - Introduced a new `Input` component for flexible input handling, supporting both text and file types with appropriate styling and binding.

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,25 +1,34 @@
 <script>
   import * as Tabs from '$lib/components/ui/tabs/index.js';
   import { ArrowUpDown, BicepsFlexed, Dumbbell, Flame } from '@lucide/svelte';
+  import { page } from '$app/state';
 </script>
 
-<Tabs.Root value="converter">
-  <Tabs.List>
-    <Tabs.Trigger value="converter">
-      <ArrowUpDown class="h-4 w-4" />
-      <a href="/">Converter</a>
+<Tabs.Root value={page.url.pathname.split('/')[1]}>
+  <Tabs.List class="w-full">
+    <Tabs.Trigger value="" class="p-0">
+      <a href="/" class="flex h-full w-full items-center justify-center gap-1 p-1">
+        <ArrowUpDown class="h-4 w-4" />
+        Converter
+      </a>
     </Tabs.Trigger>
-    <Tabs.Trigger value="disk">
-      <Dumbbell class="h-4 w-4" />
-      <a href="/disks">Disks</a>
+    <Tabs.Trigger value="plates" class="p-0">
+      <a href="/plates" class="flex h-full w-full items-center justify-center gap-1 p-1">
+        <Dumbbell class="h-4 w-4" />
+        Plates
+      </a>
     </Tabs.Trigger>
-    <Tabs.Trigger value="warmup">
-      <Flame class="h-4 w-4" />
-      <a href="/warmup">Warmup</a>
+    <Tabs.Trigger value="warmup" class="p-0">
+      <a href="/warmup" class="flex h-full w-full items-center justify-center gap-1 p-1">
+        <Flame class="h-4 w-4" />
+        Warmup
+      </a>
     </Tabs.Trigger>
-    <Tabs.Trigger value="max-rep">
-      <BicepsFlexed class="h-4 w-4" />
-      <a href="/max-rep">Max Rep</a>
+    <Tabs.Trigger value="max-rep" class="p-0">
+      <a href="/max-rep" class="flex h-full w-full items-center justify-center gap-1 p-1">
+        <BicepsFlexed class="h-4 w-4" />
+        Max Rep
+      </a>
     </Tabs.Trigger>
   </Tabs.List>
 </Tabs.Root>
